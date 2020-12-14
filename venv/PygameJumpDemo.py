07e6c146449d07e43da5725f773e3b20fb62b894
Andrew Stafford <astafford2@bsu.edu>
2020-12-14 16:12:02 -0500

    Remove unneeded color setup (red)

--- venv/PygameJumpDemo.py
+++ venv/PygameJumpDemo.py
@@ -78,7 +78,6 @@ class SpriteSheetImage(pygame.sprite.Sprite):
 ##########################################
 black = (0, 0, 0)
 white = (255, 255, 255)
-red = (255, 0, 0)
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("PyGame Jumping Demo")
